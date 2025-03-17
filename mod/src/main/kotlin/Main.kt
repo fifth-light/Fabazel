7f7d748ecf03d14a956b49223dd390d4b5e0056d
fifth_light <liu-yu-jie@foxmail.com>
2025-03-17 22:48:39 +0800

    Added Fabric API and Fabric Language Kotlin (But not mapped)

--- mod/src/main/kotlin/Main.kt
+++ mod/src/main/kotlin/Main.kt
@@ -1,15 +1,27 @@
 package top.fifthlight.fabazel
 
 import net.fabricmc.api.ClientModInitializer
+// import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
 import net.minecraft.MinecraftVersion
 import net.minecraft.item.Items
+import net.minecraft.util.Util
 import org.lwjgl.glfw.GLFW
+import org.slf4j.LoggerFactory
 
-class Fabazel : ClientModInitializer {
+object Fabazel : ClientModInitializer {
+    private val logger = LoggerFactory.getLogger(Fabazel::class.java)
+
     override fun onInitializeClient() {
-        println("Hello, Bazel!")
-        println("Item dirt's name is: ${Items.DIRT.name}")
-        println("Detected version: ${MinecraftVersion.create().name}")
-        println("Calling to GLFW: ${GLFW.glfwGetPlatform()}")
+        logger.info("Hello, Bazel!")
+        logger.info("Item dirt's name is: {}", Items.DIRT.name)
+        logger.info("Detected version: {}", MinecraftVersion.create().name)
+        if (Util.getFormattedCurrentTime() == "") {
+            // Don't make a real call, just test whether this can be compiled
+            GLFW.glfwGetTime()
+        }
+        // Can't compile before remapping dependencies
+        //ClientLifecycleEvents.CLIENT_STARTED.register {
+        //    logger.info("Client loaded!")
+        //}
     }
 }
